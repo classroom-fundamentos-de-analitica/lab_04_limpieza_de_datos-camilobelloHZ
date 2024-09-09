d2a1ddcad929e128ae5c984a83c41f18da78eedb
camilo <cbelloh@unal.edu.co>
2024-09-08 23:20:58 -0500

    intento 2

--- pregunta.py
+++ pregunta.py
@@ -13,63 +13,62 @@ def clean_data():
 
     
     # Inserte su código aquí
-    def limpiar_sexo(df):
-        df = df.copy()
-        df.sexo = df.sexo.str.lower()
-        return df
+    def limpiar_genero(datos):
+        datos = datos.copy()
+        datos.sexo = datos.sexo.str.lower()
+        return datos
 
-    def limpiar_emprendimiento(df):
-        df = df.copy()
-        df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
-        return df
+    def limpiar_tipo_negocio(datos):
+        datos = datos.copy()
+        datos["tipo_de_emprendimiento"] = datos["tipo_de_emprendimiento"].str.lower()
+        return datos
     
-    def limpiar_barrio(df):
-        df = df.copy()
-        df["barrio"] = df["barrio"].str.lower()
-        return df
+    def limpiar_zona(datos):
+        datos = datos.copy()
+        datos["barrio"] = datos["barrio"].str.lower()
+        return datos
     
-    def limpiar_idea(df):
-        df = df.copy()
-        df["idea_negocio"] = df["idea_negocio"].str.lower().str.strip()
-        return df
+    def limpiar_concepto(datos):
+        datos = datos.copy()
+        datos["idea_negocio"] = datos["idea_negocio"].str.lower().str.strip()
+        return datos
     
-    def limpiar_credito(df):
-        df = df.copy()
-        df["línea_credito"] = df["línea_credito"].str.lower().str.strip()
-        return df
+    def limpiar_linea_credito(datos):
+        datos = datos.copy()
+        datos["línea_credito"] = datos["línea_credito"].str.lower().str.strip()
+        return datos
 
-    def limpiar_comuna(df):
-        df = df.copy()
-        df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
-        return df
+    def limpiar_comunidad(datos):
+        datos = datos.copy()
+        datos["comuna_ciudadano"] = datos["comuna_ciudadano"].astype(int)
+        return datos
     
-    def limpiar_fecha(df):
-        df = df.copy()
-        df["fecha_de_beneficio"] = pd.to_datetime(
-        df.fecha_de_beneficio, format="%d/%m/%Y", errors="coerce"
-        ).fillna(pd.to_datetime(df.fecha_de_beneficio, format="%Y/%m/%d", errors="coerce"))
-        return df
+    def limpiar_fecha_beneficio(datos):
+        datos = datos.copy()
+        datos["fecha_de_beneficio"] = pd.to_datetime(
+        datos.fecha_de_beneficio, format="%d/%m/%Y", errors="coerce"
+        ).fillna(pd.to_datetime(datos.fecha_de_beneficio, format="%Y/%m/%d", errors="coerce"))
+        return datos
     
-    def limpiar_monto(df):
-        df = df.copy()
-        df.monto_del_credito = df.monto_del_credito.str.rstrip()
-        df.monto_del_credito = df.monto_del_credito.replace("[,$]", "", regex=True)
-        df.monto_del_credito = df.monto_del_credito.replace("(\\.00$)", "", regex=True)
-        df.monto_del_credito = df.monto_del_credito.astype(float)
-        return df
+    def limpiar_cantidad_credito(datos):
+        datos = datos.copy()
+        datos.monto_del_credito = datos.monto_del_credito.str.rstrip()
+        datos.monto_del_credito = datos.monto_del_credito.replace("[,$]", "", regex=True)
+        datos.monto_del_credito = datos.monto_del_credito.replace("(\\.00$)", "", regex=True)
+        datos.monto_del_credito = datos.monto_del_credito.astype(float)
+        return datos
     
-    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
-    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
-    df["sexo"] = df["sexo"].str.lower()
-    df = limpiar_emprendimiento(df)
-    df = limpiar_barrio(df)
-    df = limpiar_idea(df)
-    df = limpiar_credito(df)
-    df = limpiar_comuna(df)
-    df = limpiar_fecha(df)
-    df = limpiar_monto(df)
-    df = df.drop_duplicates().dropna()
-    return df
+    datos = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    datos = datos.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    datos["sexo"] = datos["sexo"].str.lower()
+    datos = limpiar_tipo_negocio(datos)
+    datos = limpiar_zona(datos)
+    datos = limpiar_concepto(datos)
+    datos = limpiar_linea_credito(datos)
+    datos = limpiar_comunidad(datos)
+    datos = limpiar_fecha_beneficio(datos)
+    datos = limpiar_cantidad_credito(datos)
+    datos = datos.drop_duplicates().dropna()
+    return datos
 
-print(clean_data().sexo.value_counts().to_list() )
-
+print(clean_data().sexo.value_counts().to_list())
